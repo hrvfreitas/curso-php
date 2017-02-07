77ddcd7404b7cbf34c9529ce7850cabe69b4152f
php 12 <php.12@metralha.cec.ime.usp.br>
2017-02-07 19:32:15 -0200

    correção exercicicios ontem

--- logica/exercicio-3-corrigido.php
+++ logica/exercicio-3-corrigido.php
@@ -1,11 +1,9 @@
 <?php
-
-
+// corrigido
 for($i = 1; $i <= 100; $i++ ) {
-
-if($i % 2 == 1) {
-    echo "*" . PHP_EOL;
-} else {
+  if($i % 2 == 1) {
+    echo "$i - *" . PHP_EOL;
+  } else {
     echo "$i - **" . PHP_EOL;
   }
-} ?>
+}
