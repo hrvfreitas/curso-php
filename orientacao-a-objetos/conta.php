a94f390631b4c0ab16fea90d75e4f8701b07c4a5
php 12 <php.12@metralha.cec.ime.usp.br>
2017-02-07 20:36:35 -0200

    adicionando nota

---
+++ orientacao-a-objetos/conta.php
@@ -0,0 +1,16 @@
+<?php
+  class Conta {
+
+      public $numeros;
+      public $saldo;
+
+  }
+
+$conta = new Conta;
+$conta->numero = 123;
+$conta->saldo = 550;
+
+echo "Numero da conta: $conta->numero\n";
+echo "Saldo: $conta->saldo\n;"
+
+ ?>
