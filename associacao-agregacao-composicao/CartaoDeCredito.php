dd55546a44e43459bb900176f461495a5188e036
php 12 <php.12@metralha.cec.ime.usp.br>
2017-02-07 22:42:09 -0200

    exercicio agragação

--- associacao-agregacao-composicao/CartaoDeCredito.php
+++ associacao-agregacao-composicao/CartaoDeCredito.php
@@ -1,6 +1,22 @@
 <?php
-  class cartaoDeCredito {
-    public
+Class Cliente{
+  public $nome;
+  public $codigo;
 
-    //
-  }
+
+}
+Class CartaoDeCredito{
+  public $numero;
+  public $datadeValidade;
+  public $dono;
+
+}
+$cliente = new Cliente;
+$cliente->nome = 'Rafael Cosentino';
+$cliente->codigo = 123;
+$cartao = new CartaoDeCredito;
+$cartao->numero = 111111;
+$cartao->datadeValidade = '12/12/18';
+$cartao->dono = $cliente;
+
+echo "Nome Dono do cartão: {$cartao->dono->nome}\n";
