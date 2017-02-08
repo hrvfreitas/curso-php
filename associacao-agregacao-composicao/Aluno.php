9a89e31f1565ce17161676a7dffba91428701830
php 12 <php.12@metralha.cec.ime.usp.br>
2017-02-07 22:50:29 -0200

    atualizando exercicio agragação

---
+++ associacao-agregacao-composicao/Aluno.php
@@ -0,0 +1,21 @@
+<?php
+Class Aluno{
+  public $nome;
+  public $rg;
+  public $dataDeNascimento;
+}
+Class Turma{
+  public $periodo;
+  public $serie;
+  public $sigla;
+  public $tipoDeEnsino;
+}
+$aluno = new Aluno;
+$aluno->nome = "Rafael Cosentino";
+$aluno->rg = 111111;
+$aluno->dataDeNascimento = 30/10/1984;
+$turma->perido = "Manha";
+$serie-> "5"
+$siga-> "B"
+$tipoDeEnsino->
+echo "Nome do dono do cartão: {$cartao->dono->nome}\n";
