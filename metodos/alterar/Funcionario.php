80368aad4af84ba2020993af2d8a2457da54cbdf
php 12 <php.12@metralha.cec.ime.usp.br>
2017-02-08 21:59:43 -0200

    atualizando exercicio agragação

---
+++ metodos/alterar/Funcionario.php
@@ -0,0 +1,17 @@
+<?php
+
+
+  class Funcionario {
+
+    public $nome;
+    public $salario;
+    public $valorAumento;
+    public function aumenta($valor);
+    {
+      $this->$salarioAumento (salario + $valor);
+    }
+    public function consultaDados($valor);
+    {
+      Return $this->$salarioAumento;
+  }
+ ?>
