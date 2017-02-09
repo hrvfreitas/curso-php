4c77f3036c8765cae26ccdddfcce074a15770b33
php 12 <php.12@metralha.cec.ime.usp.br>
2017-02-08 22:30:27 -0200

    atualizando array

---
+++ arrays/TesteArrays.php
@@ -0,0 +1,22 @@
+<?php
+$cursos = array();
+
+$curso[] = "lógica de Programação com Java"; //adicionando $array
+
+//$cursos = "Calculo";
+
+//$cursos = array("Calculo1", "Calculo2", "Algelin ") //adicionando vários Array
+
+//var_dump($cursos);
+
+foreach ($cursos as $x =>) {  //percorre os valores
+  echo "chave: $x, valor: $valor" . PHP_EOL;
+}
+
+//echo "$cursos[0]" . PHP_EOL; //pega pela chave
+//for($i =0; $i < count($cursor); $i++) {
+//  echo "Cursos $cursos[i]" . PHP.EOL;
+//}
+
+
+ ?>
