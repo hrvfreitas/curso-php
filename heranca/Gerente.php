bb501fa220d717a6cc15e2e12d6d91c6328950f1
php 12 <php.12@metralha.cec.ime.usp.br>
2017-02-10 22:19:43 -0200

    laravel

--- heranca/Gerente.php
+++ heranca/Gerente.php
@@ -8,6 +8,10 @@ class Gerente extends Funcionario
 
   public $usuario;
   public $senha;
+  public function calculaBonus()
+  {
+    return $this->salario * 0.4 + 2*self::PORCENTAGEM_DO_BONUS
+  }
 }
 
  ?>
